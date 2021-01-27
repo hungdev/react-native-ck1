6d20a997ad3b4f69f456d715e2a982aeb36b7eb1
Cee <hungns126@gmail.com>
2021-01-27 19:30:24 +0700

    add redux

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-import { Provider } from 'react-redux';
-import { createStore, } from 'redux';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducer from "./src/reducers";
 
 import Home from "./src/screens/Home";
 
-let store = createStore();
+let store = createStore(allReducer);
 
 export default function App() {
   return (
     <Provider store={store}>
       <Home />
     </Provider>
-  )
+  );
 }
