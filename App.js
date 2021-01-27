4fea19da5532e5c14b15bd993ebec8b912a119a2
Cee <hungns126@gmail.com>
2021-01-27 20:30:20 +0700

    add UI home, wl

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import allReducer from "./src/reducers";
 
-import Home from "./src/screens/Home";
+import AppContainer from "./src/appNavigation/Router";
 
 let store = createStore(allReducer);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <AppContainer />
     </Provider>
   );
 }
